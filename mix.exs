e7252824009b4b44285327207f4c670da09e157e
Muhammad Tanweer <tanweerdev@gmail.com>
2020-07-29 13:55:13 +0500

    Updates related to mix file

--- mix.exs
+++ mix.exs
@@ -5,37 +5,58 @@ defmodule PhEctoAcl.MixProject do
     [
       app: :phecto_acl,
       version: "0.1.0",
-      build_path: "../../_build",
-      config_path: "config/config.exs",
-      deps_path: "../../deps",
-      lockfile: "../../mix.lock",
       elixir: "~> 1.9",
-      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      build_embedded: Mix.env() == :prod,
+      deps: deps(),
+      name: "PhEctoAcl",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      description: description(),
+      package: package(),
+      docs: [
+        # The main page in the docs
+        main: "readme",
+        # logo: "path/to/logo.png",
+        extras: ["README.md"]
+      ],
+      source_url: "https://github.com/tanweerdev/phecto_acl"
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      mod: {PhEctoAcl, []},
       extra_applications: [:logger]
     ]
   end
 
-  defp elixirc_paths(:test), do: ["lib", "test"]
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:core, in_umbrella: true}
       {:ecto, "~> 3.2"},
       {:ecto_sql, "~> 3.3"},
       {:plug, "~> 1.8.3"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true}
+    ]
+  end
+
+  defp description() do
+    "phecto_acl provides ACL layer for phoenix routes and ecto queries"
+  end
+
+  defp package() do
+    [
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/tanweerdev/phecto_acl",
+        "Docs" => "https://hexdocs.pm/phecto_acl/"
+      }
     ]
   end
 end
